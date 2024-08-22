2ecd59f51d955aee5e2f6a3c734f2061d4099ce7
jambi <jambi@mac.local>
2024-08-22 15:16:25 +0300

    Install schema generator

---
+++ schema/schema.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+        "encoding/json"
+        "fmt"
+        "github.com/invopop/jsonschema"
+        "rofl/config"
+        "os"
+)
+
+func main() {
+        schemaJSON, err := json.MarshalIndent(jsonschema.Reflect(&config.Config{}), "", "  ")
+        if err != nil {
+                fmt.Println("Error marshaling JSON schema:", err)
+                return
+        }
+        if file, err := os.Create("schema.json"); err != nil {
+                fmt.Println("Error creating file:", err)
+        } else {
+                defer file.Close()
+                if _, err := file.Write(schemaJSON); err != nil {
+                        fmt.Println("Error writing to file:", err)
+                } else {
+                        fmt.Println("JSON schema successfully saved to schema.json")
+                }
+        }
+}
